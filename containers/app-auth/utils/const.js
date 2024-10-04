9a71e64eee9ccabf86a9a51f5e033799ce92da3f
Sahil Phule <sahilph@users.noreply.github.com>
2024-10-04 04:24:59 -0400

    Add https to app-auth and app-proxy

--- containers/app-auth/utils/const.js
+++ containers/app-auth/utils/const.js
@@ -1,30 +1,51 @@
+const fs = require("fs");
+
 function readFromEnvOrTerminate(key) {
-	const value = process.env[key];
+    const value = process.env[key];
 
-	if(typeof(value) !== "string" || value.trim().length === 0) {
-		console.error(`The env. variable '${key}' is not set. Terminating...`);
+    if (typeof (value) !== "string" || value.trim().length === 0) {
+        console.error(`The env. variable '${key}' is not set. Terminating...`);
 
-		process.exit(0);
-	}
+        process.exit(0);
+    }
 
-	return value;
+    return value;
+}
+
+function readFromFsOrTerminate(value) {
+    fs.stat(value, function (err, stat) {
+        if (err != null) {
+            if (err.code === 'ENOENT') {
+                // file does not exist
+                console.error(`The file '${value}' does not exist. Terminating...`);
+            } else {
+                console.error('Some error occured. Error Code: ', err.code);
+            }
+            process.exit(1);
+        }
+    });
+    return value;
 }
 
 module.exports = Object.freeze({
-	UMBREL_COOKIE_NAME: "UMBREL_SESSION",
+    UMBREL_COOKIE_NAME: "UMBREL_SESSION",
 
-	LOG_LEVEL: process.env.LOG_LEVEL || "info",
+    LOG_LEVEL: process.env.LOG_LEVEL || "info",
 
-	PORT: parseInt(process.env.PORT) || 2000,
+    PORT: parseInt(process.env.PORT) || 2000,
 
-	UMBREL_AUTH_SECRET: readFromEnvOrTerminate("UMBREL_AUTH_SECRET"),
+    UMBREL_AUTH_SECRET: readFromEnvOrTerminate("UMBREL_AUTH_SECRET"),
 
-	TOR_PATH: process.env.TOR_PATH || "/var/lib/tor",
-	APP_DATA_PATH: process.env.APP_DATA_PATH || "/app-data",
+    TOR_PATH: process.env.TOR_PATH || "/var/lib/tor",
+    APP_DATA_PATH: process.env.APP_DATA_PATH || "/app-data",
 
-	MANAGER_IP: readFromEnvOrTerminate("MANAGER_IP"),
-	MANAGER_PORT: parseInt(readFromEnvOrTerminate("MANAGER_PORT")),
+    MANAGER_IP: readFromEnvOrTerminate("MANAGER_IP"),
+    MANAGER_PORT: parseInt(readFromEnvOrTerminate("MANAGER_PORT")),
 
-	DASHBOARD_IP: readFromEnvOrTerminate("DASHBOARD_IP"),
-	DASHBOARD_PORT: parseInt(readFromEnvOrTerminate("DASHBOARD_PORT")),
+    DASHBOARD_IP: readFromEnvOrTerminate("DASHBOARD_IP"),
+    DASHBOARD_PORT: parseInt(readFromEnvOrTerminate("DASHBOARD_PORT")),
+
+    HTTPS_KEY_PATH: readFromFsOrTerminate(process.env.HTTPS_KEY_PATH || "/ssl/ssl.key"),
+    HTTPS_CERT_PATH: readFromFsOrTerminate(process.env.HTTPS_CERT_PATH || "/ssl/ssl.cert"),
+
 });
--- containers/app-proxy/utils/const.js
+++ containers/app-proxy/utils/const.js
@@ -33,6 +33,21 @@ function readFromEnvOrTerminate(key) {
 	return value;
 }
 
+function readFromFsOrTerminate(value) {
+    fs.stat(value, function (err, stat) {
+        if (err != null) {
+            if (err.code === 'ENOENT') {
+                // file does not exist
+                console.error(`The file '${value}' does not exist. Terminating...`);
+            } else {
+                console.error('Some error occured. Error Code: ', err.code);
+            }
+            process.exit(1);
+        }
+    });
+    return value;
+}
+
 function cleanHttpPaths(str) {
 	return str.split(/[, ]+/)
 		.map(path => path.trim())
@@ -63,4 +78,7 @@ module.exports = Object.freeze({
 
 	MANAGER_IP: readFromEnvOrTerminate("MANAGER_IP"),
 	MANAGER_PORT: parseInt(process.env.MANAGER_PORT) || 3006,
+
+	HTTPS_KEY_PATH: readFromFsOrTerminate(process.env.HTTPS_KEY_PATH || "/ssl/ssl.key"),
+	HTTPS_CERT_PATH: readFromFsOrTerminate(process.env.HTTPS_CERT_PATH || "/ssl/ssl.cert"),
 });
